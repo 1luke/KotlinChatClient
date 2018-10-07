52c141e8fcf1b3873726f91d4ef3227c29709f77
1luke <lukas.dagne@gmail.com>
2018-10-07 12:11:41 +0300

    Add recycler view in MainActivity, Implement adapter(data source) requirements.

--- app/src/main/java/com/example/luke/kotlinchatclient/MainActivity.kt
+++ app/src/main/java/com/example/luke/kotlinchatclient/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.luke.kotlinchatclient
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.Menu
 import android.view.MenuItem
 
@@ -19,6 +21,16 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // Set data source
+        recyclerView.adapter = WordListAdapter(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/com/example/luke/kotlinchatclient/WordListAdapter.kt
+++ app/src/main/java/com/example/luke/kotlinchatclient/WordListAdapter.kt
@@ -2,12 +2,16 @@ package com.example.luke.kotlinchatclient
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 
-class WordListAdapter internal constructor(context: Context):
+class WordListAdapter internal constructor(val context: Context):
         RecyclerView.Adapter<WordListAdapter.WordViewHolder>() {
+
+    // Cache
+    private var words = emptyList<Word>()
 
     inner class WordViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         val wordItemView: TextView = itemView.findViewById(R.id.textView)
@@ -15,15 +19,25 @@ class WordListAdapter internal constructor(context: Context):
 
     // MARK: RecyclerView.Adapter
 
+    // cellForRow
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
-        throw Exception("TODO:")
+       return WordViewHolder(
+               LayoutInflater.from(context).inflate(R.layout.recyclerview_item, parent, false)
+       )
     }
 
+    /// willDisplayCell
     override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
-        //
+        holder.wordItemView.text = words[position].word
     }
 
-    override fun getItemCount(): Int {
-        throw Exception("TODO:")
+    /// numberOfItems
+    override fun getItemCount(): Int = words.size
+
+    // MARK: Internal
+
+    internal fun setWords(words: List<Word>) {
+        this.words = words
+        notifyDataSetChanged()
     }
 }
