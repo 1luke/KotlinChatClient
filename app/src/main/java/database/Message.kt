523da90049d9769f6f85c6531c4ce10bf39dd153
1luke <lukas.dagne@gmail.com>
2018-10-07 22:34:50 +0300

    Message + MessageDao.

--- app/src/main/java/database/Message.kt
+++ app/src/main/java/database/Message.kt
@@ -1,8 +1,25 @@
 package com.example.luke.kotlinchatclient
 
-import android.arch.persistence.room.ColumnInfo
-import android.arch.persistence.room.Entity
-import android.arch.persistence.room.PrimaryKey
+import android.arch.persistence.room.*
 
-@Entity(tableName = "message")
-data class Message(@PrimaryKey val word: String)
+@Entity(tableName = "messages")
+data class Message(@PrimaryKey
+                   val id: String,
+                   val timestamp: Long,
+                   val type: String,
+                   val content: String,
+                   val senderId: String,
+                   val sender: String
+)
+
+@Dao
+interface MessageDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(message: Message)
+
+    @Query("SELECT * from messages ORDER BY timestamp ASC")
+    fun messages(): List<Message>
+
+    @Query("SELECT * FROM messages")
+    fun deleteAll()
+}
