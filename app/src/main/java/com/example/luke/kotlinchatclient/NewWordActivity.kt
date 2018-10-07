26ea586c90d66ac9acfb9646096a316c248c31a3
1luke <lukas.dagne@gmail.com>
2018-10-07 12:53:07 +0300

    Add NewWordActivity.

---
+++ app/src/main/java/com/example/luke/kotlinchatclient/NewWordActivity.kt
@@ -0,0 +1,12 @@
+package com.example.luke.kotlinchatclient
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NewWordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_word)
+    }
+}
